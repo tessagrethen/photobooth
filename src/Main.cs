97c1610c65caa17fd3628fd0503ce27639474b19
Tessa Pham <tessagrethen@gmail.com>
2019-09-29 17:39:28 -0400

    removed comments

--- src/Main.cs
+++ src/Main.cs
@@ -8,15 +8,6 @@ namespace Photobooth
         static void Main(string[] args)
         {
             Application.Init();
-
-            // Replace the following code with your MainWindow
-            // Window window = new Window("Empty Window");
-            // window.DeleteEvent += delegate { Application.Quit(); };
-            // window.ShowAll();
-
-            // Application.Run();
-
-            Application.Init();
             MainWindow mainWindow = new MainWindow("Photobooth");
             mainWindow.ShowAll();
             Application.Run();
